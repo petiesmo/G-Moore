3d21f554933a13bc686bf8d7b3bd195720e6f070
petiesmo <66927015+petiesmo@users.noreply.github.com>
2021-03-01 17:30:04 -0700

    Foundations for User-selected sites (from dB/file)

--- scratch/scripts.js
+++ scratch/scripts.js
@@ -1,10 +1,10 @@
 //scripts.js
 
 function csvToJson(csv) {
-  //Converts csv string to Json string; accepts quoted values  
+  //Converts csv string to Json (ARRAY) string; accepts quoted values  
   var lines = csv.split("\n");
   var result = [];
-  var headers = lines[0].split(",");
+  var headers = lines[0].replace(/(\r\n|\n|\r)/gm, "").split(",");
 
   for(var i=1; i<lines.length; i++) {
     var obj = {};
@@ -42,33 +42,71 @@ function csvToJson(csv) {
     }
     result.push(obj);
   }
-  return result;
-  
-  /* return result; //JavaScript object */
-  /* return JSON.stringify(result); //JSON */
+  return JSON.stringify(result);
 }
 
-/* async function fetchLocal(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest
-    xhr.onload = function() {
-      resolve(new Response(xhr.responseText, {status: xhr.status}))
-    }
-    xhr.onerror = function() {
-      reject(new TypeError('Local request failed'))
-    }
-    xhr.open('GET', url)
-    xhr.send(null)
-  })
-} */
-/* csvstring = fetchLocal('./sites.csv')
-console.log(csvstring) */
 async function getSites(){
   let response = await fetch('./sites.csv');
   let data = await response.text();
-  //catch(error => console.error(error));
-  console.log(data)
-  return data
+  let jdata = csvToJson(data);
+  console.log(jdata)
+  return jdata
 }
-let Sites = getSites();
-document.getElementById("data1").innerText = Sites;
+getSites()
+  .then(Sites => JSON.parse(Sites))
+  //.then(aSites => jsonToTable(aSites,"data1"))
+  .then(aSites => makeSelectFromJson(aSites,"ChooseSites"))
+  .catch(error => console.error(error));
+
+function jsonToTable(jsonArray, divtag="data")   {
+    // EXTRACT VALUE FOR HTML HEADER. 
+    var col = [];
+    for (var i = 0 in jsonArray) {
+        for (var key in jsonArray[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+    for (var i = 0 in col) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0 in jsonArray) {
+        tr = table.insertRow(-1);
+        for (var j in col) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = jsonArray[i][col[j]];
+        }
+    }
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById(divtag);
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+  }
+
+function makeSelectFromJson(jsonArray, selectId){
+  var dropdown = document.createElement("select");
+  dropdown.id = selectId
+  jsonArray.forEach(elem => {
+    console.log(elem)
+    var opt = document.createElement("option");
+    opt.textContent = elem.Site + " - " + elem.Pad;
+    opt.value = JSON.stringify({lat: elem.Latitude, lng: elem.Longitude})
+    dropdown.appendChild(opt);
+  });
+  var divContainer = document.getElementById("Select1");
+  //divContainer.innerHTML = "";
+  divContainer.appendChild(dropdown);
+  dropdown.onchange = displayValue();
+  }
+
+  function displayValue(){
+    document.getElementById("chosen").innerText = document.getElementById("ChooseSites").value;
+  }
