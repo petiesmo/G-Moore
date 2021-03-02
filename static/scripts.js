f75f3d8a90f641a5eb182bbadf41e0131b2dda57
petiesmo <66927015+petiesmo@users.noreply.github.com>
2021-03-02 14:16:37 -0700

    Still debugging dropdown to form

--- static/scripts.js
+++ static/scripts.js
@@ -1,24 +1,26 @@
 //scripts.js
+//Modulo (remainder) division, accommodating negative numbers
+function modulo(n, m) {
+  return ((n % m) + m) % m;
+} 
 
-function csvToJson(csv) {
+function csvToJsonArray(csv) {
   //Converts csv string to Json (ARRAY) string; accepts quoted values  
   var lines = csv.split("\n");
   var result = [];
-  var headers = lines[0].replace(/(\r\n|\n|\r)/gm, "").split(",");
+  var headers = lines[0].trim().split(",");
 
-  for(var i=1; i<lines.length; i++) {
+  for(var i=1 in lines) {
     var obj = {};
     var row = lines[i],
       queryIdx = 0,
       startValueIdx = 0,
       idx = 0;
-
     if (row.trim() === '') { continue; }
-
+    /* Step through characters in the row, segmenting by delimiter */
     while (idx < row.length) {
-      /* if we meet a double quote we skip until the next one */
+      /* If a double quote encountered, skip until the next one */
       var c = row[idx];
-
       if (c === '"') {
         do { c = row[++idx]; } while (c !== '"' && idx < row.length - 1);
       }
@@ -26,14 +28,13 @@ function csvToJson(csv) {
       if (c === ',' || /* handle end of line with no comma */ idx === row.length - 1) {
         /* Capture value */
         var value = row.substr(startValueIdx, idx - startValueIdx).trim();
-
         /* Skip first double quote */
         if (value[0] === '"') { value = value.substr(1); }
         /* Skip last comma */
         if (value[value.length - 1] === ',') { value = value.substr(0, value.length - 1); }
         /* Skip last double quote */
         if (value[value.length - 1] === '"') { value = value.substr(0, value.length - 1); }
-
+        /* Assign value to key*/
         var key = headers[queryIdx++];
         obj[key] = value;
         startValueIdx = idx + 1;
@@ -44,19 +45,22 @@ function csvToJson(csv) {
   }
   return JSON.stringify(result);
 }
-//Retrieves the location info stored in sites.csv
+//Retrieves the location info stored in sites.csv and returns as a JSON array
 /*Future: Could generalize, if other files are needed (e.g., user customer sites)*/
-async function getSites(){
-  let response = await fetch('./sites.csv');
+async function getSites(fileURL){
+  let response = await fetch(fileURL);
   let data = await response.text();
-  let jdata = csvToJson(data);
+  let jdata = csvToJsonArray(data);
   console.log(jdata)
   return jdata
 }
 
-getSites()
+//Populate the dropdown selector from a helper CSV file
+const fileSites = new File('sites.csv')
+const sitesURL = URL.createObjectURL(fileSites)
+//Now fetch that file
+getSites(sitesURL)
   .then(Sites => JSON.parse(Sites))
-  //.then(aSites => jsonToTable(aSites,"data1"))
   .then(aSites => makeSelectFromJson(aSites,"ChooseSites"))
   .catch(error => console.error(error));
 
@@ -71,12 +75,12 @@ function makeSelectFromJson(jsonArray, selectId){
     opt.value = JSON.stringify({lat: elem.Latitude, lng: elem.Longitude})
     dropdown.appendChild(opt);
   });
-  var divContainer = document.getElementById("Select1");
+  var divContainer = document.getElementById("SiteSelector2");
   //divContainer.innerHTML = "";
   divContainer.appendChild(dropdown);
-  dropdown.onchange = displayValue();
-  }
+  dropdown.onchange = SiteToForm(this);
+}
 
-  function displayValue(){
-    document.getElementById("chosen").innerText = document.getElementById("ChooseSites").value;
-  }
+function displayValue(){
+  document.getElementById("LatLng2").innerText = document.getElementById("ChooseSites").value;
+}
