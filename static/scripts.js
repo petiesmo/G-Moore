b3d71f4a915507d5027be6cc08c89942ca9b1a84
petiesmo <66927015+petiesmo@users.noreply.github.com>
2021-03-03 15:19:22 -0700

    trying same origin

--- static/scripts.js
+++ static/scripts.js
@@ -48,7 +48,7 @@ function csvToJsonArray(csv) {
 //Retrieves the location info stored in sites.csv and returns as a JSON array
 /*Future: Could generalize, if other files are needed (e.g., user customer sites)*/
 async function getSites(sitesURL){
-  let response = await fetch(sitesURL, {mode: 'no-cors'});
+  let response = await fetch(sitesURL, {mode: 'same-origin'});
   let data = await response.text();
   let jdata = csvToJsonArray(data);
   return jdata
